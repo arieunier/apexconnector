ccafb0f35e91a5b6dd463ec4fb07b35cdf479221
Augustin Rieunier <arieunier@arieunier-ltm2.internal.salesforce.com>
2020-03-12 10:51:34 +0100

    deploy heroku

---
+++ Heroku/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+# set me
+if [ $# -ne 2 ]
+then
+    echo "Usage : deploy.sh APPLICATION_NAME "
+    exit 1
+fi
+
+APPLICATION_NAME=$1
+
+echo "######### Creating the app"
+heroku apps:create $APPLICATION_NAME --region eu 
+
+echo "######### Adding Librato"
+heroku addons:create librato:free --app $APPLICATION_NAME
+
+echo "######### Adding Timber.io addon"
+heroku addons:create timber-logging --app $APPLICATION_NAME
+
+echo "######### adding other environment variables"
+
+heroku config:set LOG_LEVEL='DEBUG' --app $APPLICATION_NAME
+heroku config:set APPNAME=$APPLICATION_NAME --app $APPLICATION_NAME
+
+echo "######### Pushing sources"
+
+git push heroku master
+
+
